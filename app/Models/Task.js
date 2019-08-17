0fe6da0c58e28aff4a326af23774eaa551e89710
Edson Lima <edsonlima2178@gmail.com>
2019-08-17 07:01:06 -0300

    first commit

---
+++ app/Models/Task.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Task extends Model {
+  project () {
+    this.belongsTo('App/Models/Project')
+  }
+
+  user () {
+    this.belongsTo('App/Model/App')
+  }
+
+  file () {
+    this.belongsTo('App/Models/File')
+  }
+}
+
+module.exports = Task
